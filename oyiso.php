0df6c8b7527b6b71a7f653a95128720f088a7559
神秘布偶猫 <kannafay@gmail.com>
2025-12-04 23:59:20 +0800

    新增禁用wp更新

--- oyiso.php
+++ oyiso.php
@@ -1,8 +1,8 @@
 <?php
 /*
 Plugin Name: 橘子猫头
-Description: 橘子猫头的多功能插件
-Version:     1.0.0
+Description: 橘子猫头的多功能实用插件
+Version:     1.0.1
 Author:      橘子猫头
 Text Domain: oyiso
 Domain Path: /languages
@@ -12,7 +12,7 @@ defined('ABSPATH') || exit;
 
 add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links) {
     $settings_link = '<a href="plugins.php?page=oyiso">' . __('Settings') . '</a>';
-    array_unshift($links, $settings_link); // 放在第一个位置
+    array_unshift($links, $settings_link);
     return $links;
 });
 
--- src/1-gutenberg-editor.php
+++ src/1-gutenberg-editor.php
@@ -12,8 +12,8 @@ if (class_exists('CSF')) {
             array(
                 'id' => 'opt-gutenberg-editor',
                 'type' => 'switcher',
-                'title' => '古腾堡编辑器',
-                'label' => '启用/禁用古腾堡编辑器，禁用后将使用经典编辑器',
+                'title' => '禁用古腾堡编辑器',
+                'label' => '开启后将禁用古腾堡编辑器，使用经典编辑器',
                 'default' => false
             ),
         )
@@ -24,10 +24,9 @@ if (class_exists('CSF')) {
         return;
     }
 
-    if ($options['opt-gutenberg-editor'] == false) {
+    if ($options['opt-gutenberg-editor'] == true) {
         // 禁用古腾堡编辑器
         add_filter('use_block_editor_for_post', '__return_false', 10);
-
         // 移除古腾堡相关样式
         remove_action('wp_enqueue_scripts', 'wp_common_block_scripts_and_styles');
     }
